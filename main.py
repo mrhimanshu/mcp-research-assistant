4f779f21475349cbf0cc018b494437825ed02cf5
Himanshu . <hsaini102@gmail.com>
2025-10-26 10:44:03 +0000

    Inital Commit

---
+++ main.py
@@ -0,0 +1,12 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv(os.path.join('.env'), override=True)
+
+def main():
+    print("Hello from mcp-tutorial!")
+    print(os.getenv("OPENAI_API_KEY"))
+
+
+if __name__ == "__main__":
+    main()
